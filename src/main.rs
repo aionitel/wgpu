2ec6236caf65b04661cd2b045194d497db08dbd3
Alex Ionitel <oranggsoda@gmail.com>
2024-06-01 15:55:28 -0400

    opening window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use winit::{
+    event::*,
+    event_loop::EventLoop,
+    keyboard::{KeyCode, PhysicalKey},
+    window::WindowBuilder,
+};
+
 fn main() {
-    println!("Hello, world!");
+    run();
+}
+
+pub fn run() {
+    env_logger::init(); // When wgpu panics, it throws a generic error, while logging the real error via the log crate.
+
+    // TODO: Get rid of these ugly unwraps.
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    // Simple event loop that will exit on esc key press.
+    event_loop.run(move |event, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        state: ElementState::Pressed,
+                        physical_key: PhysicalKey::Code(KeyCode::Escape),
+                        ..
+                    },
+                ..
+            } => control_flow.exit(),
+            _ => {}
+        },
+        _ => {}
+    });
 }
